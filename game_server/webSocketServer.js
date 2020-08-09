83621b9097363876193b391785b95848fffc2fd9
yuki matsuyoshi <sento.mikaruge@gmail.com>
2020-08-09 17:31:00 +0900

    temp

--- game_server/webSocketServer.js
+++ game_server/webSocketServer.js
@@ -5,14 +5,19 @@ function createWebSocketServer(io, game) {
   
       const displayName = socket.handshake.query.displayName;
       const thumbUrl = socket.handshake.query.thumbUrl;
-  
+
       console.log('WebSocket のコネクションがありました。');
       socket.emit('start data', {});
+      // const startObj = game.newConnection(socket.id, displayName, thumbUrl);
+      // socket.emit('start data', startObj);
   
-      socket.on('disconnect', () => {});
+      socket.on('disconnect', () => {
+        // game.disconnect(socket.id);
+      });
     });
   
     const socketTicker = setInterval(() => {
+        // rootIo.volatile.emit('map data', game.getMapData); // 全員に送信
         rootIo.volatile.emit('map data', {}); // 全員に送信
     },66);
 }
--- public/javascripts/index/bundle.js
+++ public/javascripts/index/bundle.js
@@ -5758,87 +5758,157 @@ var _socket2 = _interopRequireDefault(_socket);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var gameObj = {
-    raderCanvasWidth: 500,
-    raderCanvasHeight: 500,
-    scoreCanvasWidth: 300,
-    scoreCanvasHeight: 500,
-    deg: 0,
-    myDisplayName: (0, _jquery2.default)('#main').attr('data-displayName'),
-    myThumbUrl: (0, _jquery2.default)('#main').attr('data-thumbUrl')
+  raderCanvasWidth: 500,
+  raderCanvasHeight: 500,
+  scoreCanvasWidth: 300,
+  scoreCanvasHeight: 500,
+  itemRadius: 4,
+  airRadius: 5,
+  deg: 0,
+  myDisplayName: (0, _jquery2.default)('#main').attr('data-displayName'),
+  myThumbUrl: (0, _jquery2.default)('#main').attr('data-thumbUrl'),
+  fieldWidth: null,
+  fieldHeight: null,
+  itemsMap: new Map(),
+  airMap: new Map()
 };
 
 var socketQueryParameters = 'displayName=' + gameObj.myDisplayName + '&thumbUrl=' + gameObj.myThumbUrl;
 var socket = (0, _socket2.default)((0, _jquery2.default)('#main').attr('data-ipAddress') + '?' + socketQueryParameters);
 
 function init() {
-    // ゲーム用のキャンバス
-    var raderCanvas = (0, _jquery2.default)('#rader')[0];
-    raderCanvas.width = gameObj.raderCanvasWidth;
-    raderCanvas.height = gameObj.raderCanvasHeight;
-    gameObj.ctxRader = raderCanvas.getContext('2d');
+  // ゲーム用のキャンバス
+  var raderCanvas = (0, _jquery2.default)('#rader')[0];
+  raderCanvas.width = gameObj.raderCanvasWidth;
+  raderCanvas.height = gameObj.raderCanvasHeight;
+  gameObj.ctxRader = raderCanvas.getContext('2d');
 
-    // ランキング用のキャンバス
-    var scoreCanvas = (0, _jquery2.default)('#score')[0];
-    scoreCanvas.width = gameObj.scoreCanvasWidth;
-    scoreCanvas.height = gameObj.scoreCanvasHeight;
-    gameObj.ctxScore = scoreCanvas.getContext('2d');
+  // ランキング用のキャンバス
+  var scoreCanvas = (0, _jquery2.default)('#score')[0];
+  scoreCanvas.width = gameObj.scoreCanvasWidth;
+  scoreCanvas.height = gameObj.scoreCanvasHeight;
+  gameObj.ctxScore = scoreCanvas.getContext('2d');
 
-    // 潜水艦の画像
-    var submarineImage = new Image();
-    submarineImage.src = '/images/submarine.png';
-    gameObj.submarineImage = submarineImage;
+  // 潜水艦の画像
+  var submarineImage = new Image();
+  submarineImage.src = '/images/submarine.png';
+  gameObj.submarineImage = submarineImage;
 }
 
 init();
 
 function ticker() {
-    gameObj.ctxRader.clearRect(0, 0, gameObj.raderCanvasWidth, gameObj.raderCanvasHeight); // まっさら
-    drawRadar(gameObj.ctxRader);
-    drawSubmarine(gameObj.ctxRader);
+  gameObj.ctxRader.clearRect(0, 0, gameObj.raderCanvasWidth, gameObj.raderCanvasHeight); // まっさら
+  drawRadar(gameObj.ctxRader);
+  drawSubmarine(gameObj.ctxRader);
 }
 
 setInterval(ticker, 33);
 
 function drawRadar(ctxRader) {
-    var x = gameObj.raderCanvasWidth / 2;
-    var y = gameObj.raderCanvasHeight / 2;
-    var r = gameObj.raderCanvasWidth * 1.5 / 2; // 対角線の長さの半分
+  var x = gameObj.raderCanvasWidth / 2;
+  var y = gameObj.raderCanvasHeight / 2;
+  var r = gameObj.raderCanvasWidth * 1.5 / 2; // 対角線の長さの半分
 
-    ctxRader.save(); // セーブ
+  ctxRader.save(); // セーブ
 
-    ctxRader.beginPath();
-    ctxRader.translate(x, y);
-    ctxRader.rotate(getRadian(gameObj.deg));
+  ctxRader.beginPath();
+  ctxRader.translate(x, y);
+  ctxRader.rotate(getRadian(gameObj.deg));
 
-    ctxRader.fillStyle = 'rgba(0, 220, 0, 0.5)';
+  ctxRader.fillStyle = 'rgba(0, 220, 0, 0.5)';
 
-    ctxRader.arc(0, 0, r, getRadian(0), getRadian(-30), true);
-    ctxRader.lineTo(0, 0);
+  ctxRader.arc(0, 0, r, getRadian(0), getRadian(-30), true);
+  ctxRader.lineTo(0, 0);
 
-    ctxRader.fill();
+  ctxRader.fill();
 
-    ctxRader.restore(); // 元の設定を取得
-    gameObj.deg = (gameObj.deg + 5) % 360;
+  ctxRader.restore(); // 元の設定を取得
+  gameObj.deg = (gameObj.deg + 5) % 360;
 }
 
 function drawSubmarine(ctxRader) {
-    ctxRader.save();
-    ctxRader.translate(gameObj.raderCanvasWidth / 2, gameObj.raderCanvasHeight / 2);
+  ctxRader.save();
+  ctxRader.translate(gameObj.raderCanvasWidth / 2, gameObj.raderCanvasHeight / 2);
 
-    ctxRader.drawImage(gameObj.submarineImage, -(gameObj.submarineImage.width / 2), -(gameObj.submarineImage.height / 2));
-    ctxRader.restore();
+  ctxRader.drawImage(gameObj.submarineImage, -(gameObj.submarineImage.width / 2), -(gameObj.submarineImage.height / 2));
+  ctxRader.restore();
 }
 
 socket.on('start data', function (startObj) {
-    console.log('start data came');
+  gameObj.fieldWidth = startObj.fieldWidth;
+  gameObj.fieldHeight = startObj.fieldHeight;
+  gameObj.myPlayerObj = startObj.playerObj;
 });
 
 socket.on('map data', function (compressed) {
-    console.log('map data came');
+
+  var playersArray = compressed[0];
+  var itemsArray = compressed[1];
+  var airArray = compressed[2];
+
+  gameObj.playersMap = new Map();
+  var _iteratorNormalCompletion = true;
+  var _didIteratorError = false;
+  var _iteratorError = undefined;
+
+  try {
+    for (var _iterator = playersArray[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+      var compressedPlayerData = _step.value;
+
+
+      var player = {};
+      player.x = compressedPlayerData[0];
+      player.y = compressedPlayerData[1];
+      player.playerId = compressedPlayerData[2];
+      player.displayName = compressedPlayerData[3];
+      player.score = compressedPlayerData[4];
+      player.isAlive = compressedPlayerData[5];
+      player.direction = compressedPlayerData[6];
+
+      gameObj.playersMap.set(player.playerId, player);
+
+      // 自分の情報も更新
+      if (player.playerId === gameObj.myPlayerObj.playerId) {
+        gameObj.myPlayerObj.x = compressedPlayerData[0];
+        gameObj.myPlayerObj.y = compressedPlayerData[1];
+        gameObj.myPlayerObj.displayName = compressedPlayerData[3];
+        gameObj.myPlayerObj.score = compressedPlayerData[4];
+        gameObj.myPlayerObj.isAlive = compressedPlayerData[5];
+      }
+    }
+  } catch (err) {
+    _didIteratorError = true;
+    _iteratorError = err;
+  } finally {
+    try {
+      if (!_iteratorNormalCompletion && _iterator.return) {
+        _iterator.return();
+      }
+    } finally {
+      if (_didIteratorError) {
+        throw _iteratorError;
+      }
+    }
+  }
+
+  gameObj.itemsMap = new Map();
+  itemsArray.forEach(function (compressedItemData, index) {
+    gameObj.itemsMap.set(index, { x: compressedItemData[0], y: compressedItemData[1] });
+  });
+
+  gameObj.airMap = new Map();
+  airArray.forEach(function (compressedAirData, index) {
+    gameObj.airMap.set(index, { x: compressedAirData[0], y: compressedAirData[1] });
+  });
+
+  console.log(gameObj.playersMap);
+  console.log(gameObj.itemsMap);
+  console.log(gameObj.airMap);
 });
 
 function getRadian(kakudo) {
-    return kakudo * Math.PI / 180;
+  return kakudo * Math.PI / 180;
 }
 
 /***/ }),
