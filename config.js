09e66e947be6fc25ec8a4ba55614802b2da37fe4
yuki matsuyoshi <sento.mikaruge@gmail.com>
2020-08-12 14:18:38 +0900

    Herokuの環境変数を使う

--- config.js
+++ config.js
@@ -6,12 +6,3 @@ module.exports = {
     },
     ipAddress: process.env.HEROKU_URL || 'http://localhost:8000'
 };
-
-// module.exports = {
-//   twitter: {
-//     consumerKey: 'dbrDQMLDxKB3HtKUQBfCLOxY0',
-//     consumerSecret: 'RO1PZG6Qbi7QJaY8OMeqhP3HRi91BoDTvGyBByCjtReGr24Y7J',
-//     callbackURL: 'https://intense-harbor-94892.herokuapp.com/oauth_callback'
-//   },
-//   ipAddress: 'https://intense-harbor-94892.herokuapp.com/'
-// };
